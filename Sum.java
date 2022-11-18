5038405c146747143ca4bc5c2612b3a1d4e00175
PGDACAnjali <118441814+PGDACAnjali@users.noreply.github.com>
2022-11-18 20:03:24 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		float a = 12.1f;
+		float b = 13.2f;
+		float c = 14.3f;
+		float d = a+b+c;
+		System.out.println("Sum of three float number is : " +d);
+	}
+
+}
